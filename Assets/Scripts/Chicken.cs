6f3a49be6229314fac21a7c69d7794270298ec2a
LAURA WATSON <1703025@uad.ac.uk>
2020-02-28 14:57:06 +0000

    hello

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -12,27 +12,46 @@ public class Chicken : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
 
-   // public int Score;
+    public GameObject gameOverText, restartButton, scoreText;
+
+    void Start()
+    {
+        gameOverText.SetActive(false);
+        restartButton.SetActive(false);
+        scoreText.SetActive(true);
+
+    }
+
+    public int Score;
     public TextMeshProUGUI ScoreText;
 
     //public GameObject death;
+
+
     private void OnTriggerEnter(Collider Fox)
     {
         Debug.Log("hit detected");
         // GameObject d = Instantiate(death) as GameObject;
         //d.transform.position = transform.position;
         // Destroy(Fox.gameObject);
+        gameOverText.SetActive(true);
+        restartButton.SetActive(true);
+        gameObject.SetActive(false);
+        scoreText.SetActive(false);
+
         // this.gameObject.SetActive(false);
 
-      //  Score -= 1;
-     //   ScoreText.text = "Score: " + Score.ToString();
+        //  Score -= 1;
+        //   ScoreText.text = "Score: " + Score.ToString();
 
 
-        player.transform.position = respawnPoint.transform.position;
-        Fox.transform.position = respawnPoint.transform.position;
+
+
+        // player.transform.position = respawnPoint.transform.position;
+        // Fox.transform.position = respawnPoint.transform.position;
+        // }
+
     }
-    
-
 
 
 }
